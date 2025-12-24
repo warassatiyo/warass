c2cea6cd2e907f897a8c86859eef59b7c529a1a2
warassatiyo <warassatiyo@gmail.com>
2025-12-24 23:27:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "min1.jpg",
+    min2: "min2.jpg",
+    min3: "min3.jpg",
+    min4: "min4.jpg",
+    min5: "min5.jpg",
+    min6: "min6.jpg",
+
+    max1: "max1.jpg",
+    max2: "max2.jpg",
+    max3: "max3.jpg",
+    max4: "max4.jpg",
+    max5: "max5.png",
+    max6: "max6.png",
+}
